7044e395e3a664e746911918b1bef4adeca2f86c
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-26 13:25:37 +0200

    Add files via upload

---
+++ opdracht_21_loops/multiplication-tables.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+for (let i = 1; i <= 9; i++) {
+
+    for (let j = 1; j <= 9; j++) {
+
+        let k = i * j;
+
+        console.log(`${i}` + " x " + `${j}` + " = " + k);
+    }
+
+};
+
+
